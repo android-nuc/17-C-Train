b7e96360b75cc901db1e1efecb710d6514aa1d4b
Your Name <1056992492@qq.com>
2017-10-26 22:27:09 +0800

    1

--- 图书管理系统.c
+++ 图书管理系统.c
@@ -10,63 +10,155 @@ typedef struct book{
 	char authorName[20] ;
 	int pageNumber ;
 	float price ;
-	char kind[10];
+	char kind[10] ;
 	
 	struct book * next ;
 	
-}mBook;
+}Book;
 
-mBook * readDataByTxt()
+
+/*
+Book * initSystem()
 {
+	Book * head ;
+	
+	head = (Book*)malloc(sizeof(Book));
+
+	return head ;
+}
+*/
+
+
+Book * readDataByTxt()
+{
+	char allChar[100];
 	char bookName[20] ;
 	char authorName[20] ;
-	int pageNumber ;
 	float price ;
-	char kind[10]; 
-	FILE *fp ;
-	char ch  ;
+	int pageNumber;
 	
+	char kind[10]; 
+	Book *head = NULL , *prev = NULL , *bookP = NULL ;
+	FILE *fp ;
+	//char ch ，prev ;
+	int temp = 0 , i , flag ; 
 	if((fp = fopen("D:\\mynotes\\17_c_class\\17-C-Train\\data.txt","r"))==NULL)
 	{
-		printf("open error!\n"); 
-	}
+		printf("**********************从本地文档加载数据失败****************************\n"); 
+	} 
 	else
 	{
-		ch=fgetc(fp);
-		while(ch!=EOF) //判断刚读取的字符是否是文件结束符
-    	{
-        	putchar(ch); //若不是结束符，将它输出到屏幕上显示
-        	ch=fgetc(fp); //继续从fp所指文件中读取下一个字符
-    	} //完成将fp所指文件的内容输出到屏幕上显示
-    fclose(fp); //关闭fp所指文件
+		//ch=fgetc(fp);
+		do
+		{
+			fscanf(fp,"%s%s%s%f%d",bookName,authorName,kind,&price,&pageNumber);
+			//printf("成功读取：%s %s %s %.1f %d\n",bookName,authorName,kind,price,pageNumber);
+			bookP = (Book*)malloc(sizeof(Book));
+			if(prev!=NULL)
+			{
+				prev->next = bookP;
+			}
+			if(head==NULL)
+			{
+				head = bookP;
+			}
+			strcpy(bookP->bookName,bookName);
+			strcpy(bookP->kind,kind);
+			strcpy(bookP->authorName,authorName) ;
+			bookP->price = price ;
+			bookP->pageNumber=pageNumber ;
+			bookP->next = NULL ;
+			prev = bookP ;
+			//showSelf(*bookP);	
+		}while(fgetc(fp)!=EOF);
 	}
-	return NULL ;
+	//showList(head);
+	printf("**********************已成功从本地文档加载数据*******************\n\n");
+	fclose(fp);
+	return head ;
 }
 
-void showSelf(mBook mBook)
+void showList(Book *Book)
 {
-	printf("%s\t%s\t%s\t%.1f\t%d",mBook.bookName,mBook.authorName,mBook.kind,mBook.price,mBook.pageNumber);
+	  do
+    {
+        printf("%10s   %10s   %10s    %.1f    %4d\n",Book->bookName,Book->authorName,Book->kind,Book->price,Book->pageNumber);
+    }while(Book = Book->next);
+}
+
+void showSelf(Book Book)
+{
+	printf("%s   %s   %s   %.1f   %d\n",Book.bookName,Book.authorName,Book.kind,Book.price,Book.pageNumber);
 }
 
 void showTitle()
 {
 	printf("*****************************************************************\n\n");
-	printf("                           图书管理系统\n\n			1、列出当前库存\n			2、从本地数据库中删除一本书\n			3、增添一本书到本地数据库\n			4、取出一本书\n			5、搜索\n\n");
-	printf("*****************************************************************\n");
+	printf("                           图书管理系统\n\n			1、列出当前库存\n			2、删除一本书\n			3、增添一本书\n			4、取出一本书\n			5、搜索\n			6、查看菜单\n\n");
+	printf("*****************************************************************\n\n\n");
 }
 
 int main()
 {
-	mBook * head ;
+	Book * head ;
+	int functionCode , temp  ;
 	/*
-	head = (mBook*)malloc(sizeof(mBook));
+	1、列出当前库存		
+	2、删除一本书			
+	3、增添一本书		
+	4、取出一本书		
+	5、搜索
+	head = (Book*)malloc(sizeof(Book));
 	strcpy(head->bookName,"高数");
 	strcpy(head->authorName,"王");
 	strcpy(head->kind,"math");
 	head->price = 19.2 ;
 	head->pageNumber = 300 ;
 	*/
-	//showTitle();
-	head = readDataByTxt();
+	showTitle();
+	for(temp = 0 ; temp < 100 ; temp ++)
+	{
+		if(temp == 0 )
+		{
+			head = readDataByTxt();
+		}
+		printf("请输入您所需要的操作:");
+		scanf("%d",&functionCode);
+		//printf("%d",functionCode);
+		switch(functionCode)
+		{
+			case 1 :
+				printf("\n以下为当前图书列表:\n\n%8s    %10s   %10s     %s     %s\n\n","书名","作者","种类","价钱","页数");
+				showList(head);
+				printf("\n\n");
+				break;
+			case 2 :
+				printf("\n\n");
+
+				break;
+			case 3 :
+				printf("\n\n");
+				break;
+			case 4 :
+				printf("\n\n");
+				
+				break;
+			case 5 :
+				printf("\n\n");
+				
+				break;
+			case 6 :
+				showTitle();
+				
+				break;
+			default:
+				printf("请输入1~5的数字\n");
+				
+				break; 
+		}
+	}
+	
+	//showList(head);
+
 	return 0;
  } 
